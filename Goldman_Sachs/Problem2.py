c0384af2453b059e87c7cf577d473b2f3f094303
Tejas <devtej2344@gmail.com>
2022-01-01 20:19:16 +0530

    Solved Second Problem

--- Goldman_Sachs/Problem2.py
+++ Goldman_Sachs/Problem2.py
@@ -26,6 +26,27 @@ Explanation:
 The rectangles do not overlap.
 
 """
-def doOverlap(self, L1, R1, L2, R2):
+def doOverlap(L1, R1, L2, R2):
 
-    pass
+    if L1[0]==R1[0] or L1[1]==R1[1] or L2[0]==R2[0] or L2[1]==R2[1]:
+        return 0
+
+    
+    if L1[0]>=R2[0] or L2[0]>=R1[0]:
+        return 0
+
+    if R1[1]>=L2[1] or R2[1]>=L1[1]:
+        return 0
+    
+    return 1
+
+
+L1=[0,10]
+R1=[10,0]
+L2=[5,5]
+R2=[15,0]
+
+if doOverlap(L1,R1,L2,R2):
+    print("Rectangles Overlapping")
+else:
+    print("Rectangles Don't Overlap")
